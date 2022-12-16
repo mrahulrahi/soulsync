97c0d1c696bff5f1862f8f81312c1e27d5906050
FireLiquidator <mrahulrahi@gmail.com>
2022-12-16 21:05:07 +0530

    commit

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-var input1 = document.querySelectorAll(".input-button");
-console.log(input1);
+let screen = document.getElementById("output-id");
+let buttons = document.querySelectorAll("button");
+let screenValue = "";
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    console.log("Button text is ", buttonText);
+    if (buttonText == "x") {
+      buttonText = "*";
+      screenValue += buttonText;
+      screen.value = screenValue;
+    } else if (buttonText == "x") {
+      buttonText = "*";
+      screenValue += buttonText;
+      screen.value = screenValue;
+    } else if (buttonText == "AC") {
+      screenValue = "";
+      screen.value = screenValue;
+    } else if (buttonText == "√") {
+      screen.value = eval(Math.sqrt(screenValue));
+    } else if (buttonText == "^") {
+      screen.value = eval(Math.power(screenValue));
+    } else if (buttonText == "π") {
+      screen.value = eval(Math.PI(screenValue));
+    } else if (buttonText == "!") {
+      screen.value = eval(Math.exp(screenValue));
+    } else if (buttonText == "=") {
+      screen.value = eval(screenValue);
+    } else {
+      screenValue += buttonText;
+      screen.value = screenValue;
+    }
+  });
+}
