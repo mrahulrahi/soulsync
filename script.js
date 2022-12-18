fc0df8d4da1992d27d01651e4119ebf3d35e78eb
FireLiquidator <mrahulrahi@gmail.com>
2022-12-18 16:18:15 +0530

    commit

--- script.js
+++ script.js
@@ -1,62 +1,39 @@
-let screen = document.getElementById("output-id");
-let buttons = document.querySelectorAll("button");
-let screenValue = "";
+let display = document.getElementById("output-id");
+let errorDisplay = document.getElementById("error-window-id");
+let buttons = document.getElementsByClassName("button");
 
 Array.prototype.forEach.call(buttons, function (button) {
   button.addEventListener("click", function () {
     if (
       button.textContent != "=" &&
-      button.textContent != "C" &&
+      button.textContent != "AC" &&
       button.textContent != "x" &&
       button.textContent != "÷" &&
       button.textContent != "√" &&
-      button.textContent != "x ²" &&
       button.textContent != "%" &&
       button.textContent != "<=" &&
-      button.textContent != "±" &&
-      button.textContent != "sin" &&
-      button.textContent != "cos" &&
-      button.textContent != "tan" &&
-      button.textContent != "log" &&
-      button.textContent != "ln" &&
-      button.textContent != "x^" &&
-      button.textContent != "x !" &&
+      button.textContent != "^" &&
+      button.textContent != "!" &&
       button.textContent != "π" &&
-      button.textContent != "e" &&
-      button.textContent != "rad" &&
-      button.textContent != "∘"
+      button.textContent != "e"
     ) {
       display.value += button.textContent;
     } else if (button.textContent === "=") {
       equals();
-    } else if (button.textContent === "C") {
+    } else if (button.textContent === "AC") {
       clear();
     } else if (button.textContent === "x") {
       multiply();
     } else if (button.textContent === "÷") {
       divide();
-    } else if (button.textContent === "±") {
-      plusMinus();
     } else if (button.textContent === "<=") {
       backspace();
     } else if (button.textContent === "%") {
       percent();
     } else if (button.textContent === "π") {
       pi();
-    } else if (button.textContent === "x ²") {
-      square();
     } else if (button.textContent === "√") {
       squareRoot();
-    } else if (button.textContent === "sin") {
-      sin();
-    } else if (button.textContent === "cos") {
-      cos();
-    } else if (button.textContent === "tan") {
-      tan();
-    } else if (button.textContent === "log") {
-      log();
-    } else if (button.textContent === "ln") {
-      ln();
     } else if (button.textContent === "x^") {
       exponent();
     } else if (button.textContent === "x !") {
@@ -73,11 +50,11 @@ Array.prototype.forEach.call(buttons, function (button) {
 
 function syntaxError() {
   if (
-    eval(display.value) == SyntaxError ||
-    eval(display.value) == ReferenceError ||
-    eval(display.value) == TypeError
+    eval(errorDisplay.value) == SyntaxError ||
+    eval(errorDisplay.value) == ReferenceError ||
+    eval(errorDisplay.value) == TypeError
   ) {
-    display.value == "Syntax Error";
+    errorDisplay.value == "Syntax Error";
   }
 }
 
@@ -136,10 +113,6 @@ function pi() {
   display.value = display.value * Math.PI;
 }
 
-function square() {
-  display.value = eval(display.value * display.value);
-}
-
 function squareRoot() {
   display.value = Math.sqrt(display.value);
 }
@@ -148,38 +121,10 @@ function percent() {
   display.value = display.value / 100;
 }
 
-function sin() {
-  display.value = Math.sin(display.value);
-}
-
-function cos() {
-  display.value = Math.cos(display.value);
-}
-
-function tan() {
-  display.value = Math.tan(display.value);
-}
-
-function log() {
-  display.value = Math.log10(display.value);
-}
-
-function ln() {
-  display.value = Math.log(display.value);
-}
-
 function exponent() {
   display.value += "^";
 }
 
 function exp() {
   display.value = Math.exp(display.value);
-}
-
-function radians() {
-  display.value = display.value * (Math.PI / 180);
-}
-
-function degrees() {
-  display.value = display.value * (180 / Math.PI);
 }
