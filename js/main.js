95e9dcd771713199fdff26e756459e26a653d3b3
mrahulrahi <mrahulrahi@gmail.com>
2023-05-11 16:52:00 +0530

    commit

--- js/main.js
+++
@@ -1,5 +0,0 @@
-import myJson from "../json/categories.json" assert { type: "json" };
-var categories = myJson.categories;
-var beverages = myJson.categories.beverages;
-console.log(categories);
-console.table(beverages);
