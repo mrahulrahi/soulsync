e136db52f8882e9c37f958f32e75672ae284d669
Rahul Maurya <mrahulrahi@gmail.com>
2024-05-06 04:51:26 +0000

    Register Page added

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,11 @@
-import './Login.css'
+import './Register.css'
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Link from 'next/link';
 
 
 
-export default function LoginPage() {
+export default function RegisterPage() {
   return (
     <main>
       <Header />
@@ -14,14 +14,23 @@ export default function LoginPage() {
           <div className="row">
             <div className="col-lg-5 mx-auto">
               <div className="form-box">
-                <h5>Log in</h5>
-                <p>Email or Username</p>
+                <h5>Sign in</h5>
+                <p>Register to manage your account</p>
                 <div className="row g-2">
-                  <div className="col-lg-12">
-                    <input type="email" className="form-control" placeholder="example@mydomain.com" />
+                  <div className="col-lg-6">
+                    <input type="text" className="form-control" placeholder="First Name" />
+                  </div>
+                  <div className="col-lg-6">
+                    <input type="text" className="form-control" placeholder="Last Name" />
                   </div>
                   <div className="col-lg-12">
-                    <input type="password" className="form-control" placeholder="***********" />
+                    <input type="email" className="form-control" placeholder="Email" />
+                  </div>
+                  <div className="col-lg-12">
+                    <input type="password" className="form-control" placeholder="Password" />
+                  </div>
+                  <div className="col-lg-12">
+                    <input type="password" className="form-control" placeholder="Confirm Password" />
                   </div>
 
                   <div className="col-lg-12">
@@ -29,17 +38,17 @@ export default function LoginPage() {
                       <div className="form-check">
                         <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
                         <label className="form-check-label" htmlFor="flexCheckDefault">
-                          Remember Me
+                        I Agree  <Link href="#!" className="btn-link"> Terms and Conditions </Link> 
                         </label>
                       </div>
-                      <Link href="#!" className="btn-link"> Forgot Your Password? </Link>
+                    
                     </div>
                   </div>
                   <div className="col-lg-12">
-                    <input type="submit" className="btn btn-default btn-block" value="Login" />
+                    <input type="submit" className="btn btn-default btn-block" value="Register" />
                   </div>
                   <div className="col-lg-12">
-                    <p className="not-info mt-3 text-center"> Not registered? <Link href="#!" className="btn-link"> Create a account </Link> </p>
+                    <p className="not-info mt-3 text-center"> Do you have an account? <Link href="#!" className="btn-link"> Login </Link> </p>
                   </div>
                 </div>
               </div>
