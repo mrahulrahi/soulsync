99cb1489f32958689c9e7ab6ec398535ffed153b
Rahul Maurya <mrahulrahi@gmail.com>
2024-05-10 04:20:24 +0000

    changes in sidebar

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -11,6 +11,15 @@ const Sidebar = () => {
                         <Image src="/images/logo.png" alt="logo" width={130} height={50} quality={100} />
                     </Link>
                 </div>
+                <div className="nav-body d-flex align-items-center justify-content-between">
+                <div className="nav-body-top d-flex flex-column w-100">
+                    <div className="nbt-profile-img-box mx-auto rounded-circle overflow-hidden d-flex align-items-center justify-content-center">
+                        <div className="nbt-profile-img ratio ratio-1x1 rounded-circle overflow-hidden">
+                        <Image src="/images/avatars/avatar-2.jpg" alt="logo" fill className="w-100 h-100 object-fit-cover" />
+                        </div>
+                    </div>
+                </div>
+                </div>
             </nav>
         </aside>
     )
