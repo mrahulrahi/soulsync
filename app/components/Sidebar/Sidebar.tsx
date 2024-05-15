02653802d63ab74f38469665f575ed974918d907
mrahulrahi <mrahulrahi@gmail.com>
2024-05-16 00:58:29 +0530

    change in sidebar

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -10,6 +10,7 @@ const Sidebar = () => {
                     <Link href="/" className="nav-brand">
                         <Image src="/images/logo.png" alt="logo" width={130} height={50} quality={100} />
                     </Link>
+                    <button className="border-0 bg-transparent"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M17.45 12.46a7 7 0 0 0-1-9.83a7.09 7.09 0 0 0-5.92-1.4a7 7 0 0 0-4 11.15a4.76 4.76 0 0 1 1.08 2.86v.29a2 2 0 0 0-.61 1.4v2a2 2 0 0 0 2 2v1a1 1 0 0 0 2 0v-1h2v1a1 1 0 0 0 2 0v-1a2 2 0 0 0 2-2v-2a2 2 0 0 0-.57-1.4v-.43a4.26 4.26 0 0 1 1.02-2.64M9 18.93v-2h6v2Zm6.89-7.72a6.18 6.18 0 0 0-1.46 3.72H9.56a6.67 6.67 0 0 0-1.5-3.78a5 5 0 0 1 2.84-8A5 5 0 0 1 17 8.07a4.92 4.92 0 0 1-1.11 3.14"/></svg></button>
                 </div>
                 <div className="nav-body">
                     <div className="nav-body-top d-flex flex-column w-100 align-items-center">
@@ -56,15 +57,13 @@ const Sidebar = () => {
                                     <span> Explore </span>
                                 </a>
                             </div>
-                            <div className="nav-item">
+                            <div className="nav-item active">
                                 <a href="#!" className="nav-link d-flex gap-3 align-items-center">
                                     <div className="nav-icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M21.15 2.86a2.89 2.89 0 0 0-3-.71L4 6.88a2.9 2.9 0 0 0-.12 5.47l5.24 2a.93.93 0 0 1 .53.52l2 5.25A2.87 2.87 0 0 0 14.36 22h.07a2.88 2.88 0 0 0 2.69-2l4.73-14.17a2.89 2.89 0 0 0-.7-2.97M20 5.2l-4.78 14.18a.88.88 0 0 1-.84.62a.92.92 0 0 1-.87-.58l-2-5.25a2.91 2.91 0 0 0-1.67-1.68l-5.25-2A.9.9 0 0 1 4 9.62a.88.88 0 0 1 .62-.84L18.8 4.05A.91.91 0 0 1 20 5.2" /></svg>
                                     </div>
-                                    <span> Message </span>
-                                    <span className="nav-tag"> 3</span>
+                                    <span> Message <span className="nav-tag"> 3</span></span>
+                                  
                                 </a>
                             </div>
                             <div className="nav-item">
@@ -75,15 +74,13 @@ const Sidebar = () => {
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"></path>
                                         </svg>
                                     </div>
-                                    <span> Trending </span>
+                                    <span> Trending </span> 
                                 </a>
                             </div>
                             <div className="nav-item">
                                 <a href="#!" className="nav-link d-flex gap-3 align-items-center">
                                     <div className="nav-icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M22 7.82a1.25 1.25 0 0 0 0-.19l-2-5A1 1 0 0 0 19 2H5a1 1 0 0 0-.93.63l-2 5a1.25 1.25 0 0 0 0 .19A.58.58 0 0 0 2 8a4 4 0 0 0 2 3.4V21a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-9.56A4 4 0 0 0 22 8a.58.58 0 0 0 0-.18M13 20h-2v-4h2Zm5 0h-3v-5a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v5H6v-8a4 4 0 0 0 3-1.38a4 4 0 0 0 6 0A4 4 0 0 0 18 12Zm0-10a2 2 0 0 1-2-2a1 1 0 0 0-2 0a2 2 0 0 1-4 0a1 1 0 0 0-2 0a2 2 0 0 1-4 .15L5.68 4h12.64L20 8.15A2 2 0 0 1 18 10" /></svg>
                                     </div>
                                     <span> Marketplace </span>
                                 </a>
@@ -109,9 +106,9 @@ const Sidebar = () => {
                                     <span> My Profile </span>
                                 </a>
                             </div>
-                            <hr className="horizontal-rule" />
+                            <div className="nav-hr my-2"></div>
                             <div className="nav-item">
-                                <a href="#!" className="nav-link d-flex gap-3 align-items-center">
+                                <a href="/login" className="nav-link d-flex gap-3 align-items-center">
                                     <div className="nav-icon">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
