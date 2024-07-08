3d7ac4ffa3cd2b7ee03a58ff8dfeb1dad73cbaf3
mrahulrahi <mrahulrahi@gmail.com>
2024-07-09 01:49:43 +0530

    card added

---
+++ app/components/NftCard/NftCard.tsx
@@ -0,0 +1,85 @@
+import './NftCard.css'
+
+const nftCard = () => {
+    return (
+        <>
+            <div className="nftcard-list d-flex flex-wrap">
+                <div className="nftcard-item">
+                    <div className="nftcard-box d-flex flex-column">
+                        <div className="nftcard-image">
+                            <img src="https://lh3.googleusercontent.com/pw/ABLVV86BkvPqD8HnZ2Ls3ud3Yi3r4E_bgkjrYW3s_qnv-RnOxZALZN4Qppup819MtYW54zqqJWw-BwA5Jgnsgf7EtzXZGhoBd3xJOdTvVasnlMzQiKRib1sSIMaKz-6nREmpLlyES1ovk_QhIFJ9vfsUxIs4=w1196-h898-s-no-gm?authuser=0" alt="" />
+                        </div>
+                        <div className="nftcard-text">
+                            <h4>Diya</h4>
+                            <p>Dipawali lighting Diyas.</p>
+                        </div>
+                        <div className="nftcard-cta mt-auto">
+                            <ul className="nftcard-cta-list d-flex align-items-center justify-content-between">
+                                <li className="nftcard-cta-item d-flex align-items-center"><img src="/icon-ethereum.svg"
+                                    alt="" /> 0.041 ETH</li>
+                                <li className="nftcard-cta-item d-flex align-items-center"><img src="/icon-clock.svg"
+                                    alt="" />Xiaomi Mi A1</li>
+                            </ul>
+                            <div className="nftcard-avatar d-flex align-items-center">
+                                <img src="/image-avatar.jpg" alt="" />
+                                <p>Creation of <span>Rahul Maurya</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="nftcard-item">
+                    <div className="nftcard-box d-flex flex-column">
+                        <div className="nftcard-image">
+                            <img
+                                src="https://lh3.googleusercontent.com/pw/ABLVV86TumQujzkj4fJ8-38Nu5lxnoLi1ot3gWetNGHNNKdbpk71cyBO4RNiy9-EtgT2YkTPU0rDKaXuDl0u3szuGlZdkLTBFRvodkLt-XOKxoskeqpxz068Adz0HN3BS7lhflDu4IDXw2r9HN68p461ndnx=w1196-h898-s-no-gm?authuser=0"
+                                alt="" />
+                        </div>
+                        <div className="nftcard-text">
+                            <h4>Camellia Flower</h4>
+                            <p>Camellias are plants that are part of the Theaceae family, and are commonly referred to as sasanquas.</p>
+                        </div>
+                        <div className="nftcard-cta mt-auto">
+                            <ul className="nftcard-cta-list d-flex align-items-center justify-content-between">
+                                <li className="nftcard-cta-item d-flex align-items-center"><img src="/icon-ethereum.svg"
+                                    alt="" /> 0.045 ETH</li>
+                                <li className="nftcard-cta-item d-flex align-items-center"><img src="/icon-clock.svg"
+                                    alt="" />Xiaomi Mi A1</li>
+                            </ul>
+                            <div className="nftcard-avatar d-flex align-items-center">
+                                <img src="/image-avatar.jpg" alt="" />
+                                <p>Creation of <span>Rahul Maurya</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="nftcard-item">
+                    <div className="nftcard-box d-flex flex-column">
+                        <div className="nftcard-image">
+                            <img
+                                src="https://lh3.googleusercontent.com/pw/ABLVV85p6KiTwrSlTnEpsc_rN8JVlmaRhL5JQiqtBmt_fxrUe3QZl1dSLZOMrTvqnzp90SAgQmQF5JcSt1vsP2O_djREVxyIctl0z-nFkWgf7QllhX0OH-EA5hOZS8P9sc73os_3p3cNO2ZUBJhDhdcFHRtD=w674-h898-s-no-gm?authuser=0"
+                                alt="" />
+                        </div>
+                        <div className="nftcard-text">
+                            <h4>Nerium Oleander Flower</h4>
+                            <p>Nerium oleander white, also known as kaner or bitti, is a broadleaved evergreen shrub that produces clusters of white flowers from April to September.</p>
+                        </div>
+                        <div className="nftcard-cta mt-auto">
+                            <ul className="nftcard-cta-list d-flex align-items-center justify-content-between">
+                                <li className="nftcard-cta-item d-flex align-items-center"><img src="/icon-ethereum.svg"
+                                    alt="" /> 0.045 ETH</li>
+                                <li className="nftcard-cta-item d-flex align-items-center"><img src="/icon-clock.svg"
+                                    alt="" />OnePlus Nord</li>
+                            </ul>
+                            <div className="nftcard-avatar d-flex align-items-center">
+                                <img src="/image-avatar.jpg" alt="" />
+                                <p>Creation of <span>Rahul Maurya</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default nftCard
