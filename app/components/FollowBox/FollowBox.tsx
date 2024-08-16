64247f88415e581c91fc82b31731080fe6b4d737
mrahulrahi <mrahulrahi@gmail.com>
2024-08-17 00:25:12 +0530

    follow box changes

--- app/components/FollowBox/FollowBox.tsx
+++ app/components/FollowBox/FollowBox.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import './FollowBox.css'
 
 const FollowBox = () => {
   return (
     <div className="follow-box">
-        <div className="follow-box-head d-flex">
+        <div className="follow-box-head d-flex justify-content-between">
             <h4>Who to follow</h4>
             <a href="#!">Refresh</a>
         </div>
-        <div className="follow-box-body"></div>
+        <div className="follow-box-body">
+            <div className="follow-box-row">
+                <div></div>
+                <div></div>
+                <div><a href="#!" className="btn btn-outline">Follow</a></div>
+            </div>
+        </div>
 
     </div>
   )
