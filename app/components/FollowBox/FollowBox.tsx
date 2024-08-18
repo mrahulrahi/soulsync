c99433f10e1d472c6f81d546d4b6a59dfd02ab67
mrahulrahi <mrahulrahi@gmail.com>
2024-08-18 23:08:36 +0530

    follow box done

--- app/components/FollowBox/FollowBox.tsx
+++ app/components/FollowBox/FollowBox.tsx
@@ -2,18 +2,48 @@ import './FollowBox.css'
 
 const FollowBox = () => {
   return (
-    <div className="follow-box">
-        <div className="follow-box-head d-flex justify-content-between">
-            <h4>Who to follow</h4>
-            <a href="#!">Refresh</a>
+    <div className="follow-box overflow-hidden">
+      <div className="follow-box-head d-flex justify-content-between">
+        <h4>Who to follow</h4>
+        <a href="#!">Refresh</a>
+      </div>
+      <div className="follow-box-body">
+        <div className="follow-box-row d-flex">
+          <div className="follow-box-avatar flex-shrink-0 rounded-circle overflow-hidden"><img src="/images/avatars/avatar-2.jpg" alt="" /></div>
+          <div className="follow-box-details ms-3">
+            <h5>Johnson Emma</h5>
+            <p>Australia</p>
+          </div>
+          <div className="follow-box-cta ms-auto"><a href="#!" className="btn btn-outline">Follow</a></div>
         </div>
-        <div className="follow-box-body">
-            <div className="follow-box-row">
-                <div></div>
-                <div></div>
-                <div><a href="#!" className="btn btn-outline">Follow</a></div>
-            </div>
+
+        <div className="follow-box-row d-flex">
+          <div className="follow-box-avatar flex-shrink-0 rounded-circle overflow-hidden"><img src="/images/avatars/avatar-2.jpg" alt="" /></div>
+          <div className="follow-box-details ms-3">
+            <h5>Johnson Emma</h5>
+            <p>Australia</p>
+          </div>
+          <div className="follow-box-cta ms-auto"><a href="#!" className="btn btn-outline">Follow</a></div>
         </div>
+
+        <div className="follow-box-row d-flex">
+          <div className="follow-box-avatar flex-shrink-0 rounded-circle overflow-hidden"><img src="/images/avatars/avatar-2.jpg" alt="" /></div>
+          <div className="follow-box-details ms-3">
+            <h5>Johnson Emma</h5>
+            <p>Australia</p>
+          </div>
+          <div className="follow-box-cta ms-auto"><a href="#!" className="btn btn-outline">Follow</a></div>
+        </div>
+
+        <div className="follow-box-row d-flex">
+          <div className="follow-box-avatar flex-shrink-0 rounded-circle overflow-hidden"><img src="/images/avatars/avatar-2.jpg" alt="" /></div>
+          <div className="follow-box-details ms-3">
+            <h5>Johnson Emma</h5>
+            <p>Australia</p>
+          </div>
+          <div className="follow-box-cta ms-auto"><a href="#!" className="btn btn-outline">Follow</a></div>
+        </div>
+      </div>
 
     </div>
   )
