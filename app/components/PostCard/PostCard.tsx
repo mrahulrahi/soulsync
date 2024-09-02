f2294e3e9e1616ad696c08131e7a477444d1a335
mrahulrahi <mrahulrahi@gmail.com>
2024-09-02 23:35:36 +0530

    post dropdown added

--- app/components/PostCard/PostCard.tsx
+++ app/components/PostCard/PostCard.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import './PostCard.css'
-import { FaRegImage, FaVideo } from "react-icons/fa6";
+import Image from 'next/image';
+import { FaEllipsis, FaRegImage, FaVideo } from "react-icons/fa6";
 
 const PostCard = () => {
-  return (
+    return (
 
-    
+
         <div className="mt-4">
             <div className="card shadow-sm rounded border-0 bg-white">
-              
+
                 <div className="card-header d-flex justify-content-between align-items-center bg-white">
                     <div className="d-flex align-items-center">
-                        <a href="#">
-                            <div className="bg-gradient p-1 rounded-circle">
-                                <img src="/images/avatars/avatar-2.jpg" className="border border-white rounded-circle w-30 h-30" alt="Avatar" />
+                        <a href="#" className="nbt-profile-img-box rounded-circle overflow-hidden d-flex align-items-center justify-content-center">
+                            <div className="nbt-profile-img ratio ratio-1x1 rounded-circle overflow-hidden">
+                                <Image src="/images/avatars/avatar-2.jpg" alt="logo" fill className="w-100 h-100 object-fit-cover" />
                             </div>
                         </a>
+
                         <span className="ml-3 font-weight-bold">Johnson Emma</span>
                     </div>
+               
                     <div className="dropdown">
-                        <a className="text-dark" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i className="fas fa-ellipsis-h"></i>
-                        </a>
+                        <button className="text-dark dropdown-toggle" type="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                            <FaEllipsis />
+                        </button>
                         <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
                             <a className="dropdown-item" href="#"><i className="fas fa-share-alt mr-2"></i> Share</a>
                             <a className="dropdown-item" href="#"><i className="fas fa-edit mr-2"></i> Edit Post</a>
@@ -32,13 +35,13 @@ const PostCard = () => {
                         </div>
                     </div>
                 </div>
-    
+
                 <div className="card-body">
                     <a href="/images/post/img4.jpg" data-toggle="lightbox">
                         <img src="/images/post/img4.jpg" className="img-fluid" alt="Post Image" />
                     </a>
                 </div>
-    
+
                 <div className="card-footer">
                     <div className="d-flex justify-content-between mb-3">
                         <a href="#" className="d-flex align-items-center">
@@ -83,10 +86,10 @@ const PostCard = () => {
                 </div>
             </div>
         </div>
-    
 
-    
-  )
+
+
+    )
 }
 
 export default PostCard
